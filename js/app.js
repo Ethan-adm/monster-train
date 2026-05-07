5fee28a21d025a235b9d9db2ec0fb58d0c22f169
Ethan-adm <georget.ethan.pro@gmail.com>
2026-05-07 23:16:46 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
-function rejoindrePartie() {
+function verifierInputs() {
     let pseudo = document.getElementById('pseudo').value;
     let roomCode = document.getElementById('room-code').value;
 
-    if (pseudo === "") {
-        alert("Veuillez entrer un pseudo !");
-        return;
+    if (pseudo === "" || roomCode === "") {
+        alert("Veuillez entrer un pseudo et un code de salon !");
+        return null;
     }
+    return { pseudo, roomCode };
+}
 
-    // Pour l'instant, on redirige vers game.html
-    // En multijoueur réel, on enverrait une requête au serveur ici
-    window.location.href = `game.html?pseudo=${pseudo}&room=${roomCode}`;
+function creerPartie() {
+    let data = verifierInputs();
+    if (data) {
+        window.location.href = `game.html?pseudo=${data.pseudo}&room=${data.roomCode}&action=create`;
+    }
+}
+
+function rejoindrePartie() {
+    let data = verifierInputs();
+    if (data) {
+        window.location.href = `game.html?pseudo=${data.pseudo}&room=${data.roomCode}&action=join`;
+    }
 }
